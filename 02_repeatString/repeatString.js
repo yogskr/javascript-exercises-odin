2f741ed3cd761b6dcc4f86f733ed0f3898366d00
yogskr <yogakrisanta@gmail.com>
2024-01-05 21:21:01 +0700

    Add if..else statement and for...lop inside the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let stringContainer = [];
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      stringContainer.push(string);
+    }
+    return stringContainer.join('');
+  } else if (num < 0) {
+    return 'ERROR';
+  }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
