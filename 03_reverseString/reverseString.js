b82bf58066741c4583a418249ce28109507a9068
yogskr <yogakrisanta@gmail.com>
2024-01-06 21:47:18 +0700

    Add a variable,, string.split(), array.reverse(), and array.join()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const splitArr = str.split('');
+  return splitArr.reverse().join('');
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
